87e38bdbf7fcf83906df859af25a5d8c9a12095f
Victor Hugo <vhaporfiro@gmail.com>
2025-01-25 13:12:03 -0300

    Adicionando getters and setters

--- src/main/java/br/com/VHAPorfiro/todolist/user/UserController.java
+++ src/main/java/br/com/VHAPorfiro/todolist/user/UserController.java
@@ -12,8 +12,8 @@ public class UserController {
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
         
-        System.out.println(userModel.name);
+        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
 
     }
-
 }
