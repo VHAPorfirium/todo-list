db41ad6776ecd680b18f1c523d578cdf809dab8d
Victor Hugo <vhaporfiro@gmail.com>
2025-01-25 13:21:55 -0300

    lombok

--- src/main/java/br/com/VHAPorfiro/todolist/user/UserModel.java
+++ src/main/java/br/com/VHAPorfiro/todolist/user/UserModel.java
@@ -1,40 +1,11 @@
 package br.com.VHAPorfiro.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username;
     private String name;
-    private String password;
-
-    public UserModel(){}
-
-    public UserModel(String name, String password, String username) {
-        this.name = name;
-        this.password = password;
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String password;  
 }
