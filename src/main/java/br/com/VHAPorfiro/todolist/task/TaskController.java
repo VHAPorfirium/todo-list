a5fd9612e862903ec7ecec7403897b8ac83d15aa
Victor Hugo <vhaporfiro@gmail.com>
2025-01-31 10:36:29 -0300

    Update parcial

--- src/main/java/br/com/VHAPorfiro/todolist/task/TaskController.java
+++ src/main/java/br/com/VHAPorfiro/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.VHAPorfiro.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -59,11 +60,11 @@ public class TaskController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
+        var task = this.taskRepository.findById(id).orElse(null);
 
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
 
     }
 }
